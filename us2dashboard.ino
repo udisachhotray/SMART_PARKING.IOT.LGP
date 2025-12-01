9822260bc290b8e3b85849948c0678eda3d30161
udisachhotray <udisa.chhotray_lgp25@ashoka.edu.in>
2025-12-01 18:20:02 +0530

    Add files via upload

---
+++ us2dashboard.ino
@@ -0,0 +1,52 @@
+#include <NinjaIoT.h>
+
+NinjaIoT iot;
+
+// Ultrasonic Sensor Pins
+#define TRIG_PIN D8
+#define ECHO_PIN D7
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  // Connect to WiFi and IoT account
+  iot.connect("KRISHNAISA", "Udisa1111@", "UC11");   // https://iot.roboninja.in/
+}
+
+long getDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  long distance = duration * 0.034 / 2;  // Convert to cm
+  delay(50);
+  return distance;
+}
+
+void loop() {
+
+  // Fetch cache values from cloud once per loop
+  iot.ReadAll();
+
+  // Read DISTANCE from cloud if needed (optional)
+  String serverDistance = iot.SyncVar("DISTANCE");
+  Serial.println("Distance from cloud: " + serverDistance);
+
+  // Get actual ultrasonic value
+  long distance = getDistance();
+  Serial.print("Measured Distance: ");
+  Serial.println(distance);
+
+  // Send new distance value to dashboard
+  iot.WriteVar("DISTANCE", String(distance));
+
+  delay(2000); // 5 second update rate
+}
+
